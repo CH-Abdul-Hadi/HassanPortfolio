f72ab2e56d42a9af8ce9c8ca0335598d4e55004e
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-07-28 17:15:57 +0500

    project card

--- src/components/Projects/project-card.jsx
+++ src/components/Projects/project-card.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 function ProjectCard({ project }) {
   return (
@@ -7,12 +7,14 @@ function ProjectCard({ project }) {
         <div className="h-[1px] w-full flex-shrink-0 bg-gradient-to-r from-transparent via-[#FF4081] to-[#00FFEE]"></div>
         <div className="h-[1px] w-full bg-gradient-to-r from-[#00FFEE] to-transparent"></div>
       </div>
-      <div className="px-4 lg:px-8 py-3 lg:py-5 relative">
+      {/*  */}
+      <div className="px-4 lg:px-8 mb-4 pt-3 lg:py-5 relative">
         <div className="flex flex-row space-x-1 lg:space-x-2 absolute top-1/2 -translate-y-1/2">
           <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-[#FF4081]"></div>
           <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-[#FF6EC7]"></div>
           <div className="h-2 w-2 lg:h-3 lg:w-3 rounded-full bg-[#00FFEE]"></div>
         </div>
+        {/*  */}
         <p className="text-center ml-3 text-[#FFFFFF] text-base lg:text-xl">
           {project.name}
         </p>
@@ -23,7 +25,7 @@ function ProjectCard({ project }) {
             <span className="mr-2 text-[#FF4081]">const </span>
             <span className="mr-2 text-[#FFFFFF]">project</span>
             <span className="mr-2 text-[#FF4081]">=</span>
-            <span className="text-[#FFFFFF]">{'{'}</span>
+            <span className="text-[#FFFFFF]">{"{"}</span>
           </div>
           <div>
             <span className="ml-4 lg:ml-8 mr-2 text-[#FFFFFF]">name:</span>
@@ -51,7 +53,7 @@ function ProjectCard({ project }) {
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
             <span className="text-[#FFFFFF]">Description:</span>
-            <span className="text-[#00FFEE]">{' ' + project.description}</span>
+            <span className="text-[#00FFEE]">{" " + project.description}</span>
             <span className="text-[#FFFFFF]">,</span>
           </div>
           <div>
